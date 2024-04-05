193c35af0303d4e82f62d66fff69b8b1d93b7281
glashenka20 <glafiratimchenko@gmail.com>
2024-04-05 11:18:24 +1000

    add prot

---
+++ DalsheBogaNet/Mvvm/Model/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalsheBogaNet.Mvvm.Model
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int Amount { get; set; }
+        public string Size { get; set; }
+        public bool Breakable { get; set; }
+    }
+}
---
+++ DalsheBogaNet/Mvvm/Model/ProductZapolnenie.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace DalsheBogaNet.Mvvm.Model
+{
+    class ProductZapolnenie
+    {
+        static ProductZapolnenie instance;
+        public static ProductZapolnenie Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ProductZapolnenie();
+                return instance;
+            }
+        }
+        public List<Product> GetProducts()
+        {
+            List<Product> products = new List<Product>();
+            var connect = MySqlDB.Instance.GetConnection();
+            if (connect == null)
+                return products;
+
+            string sql = "SELECT * FROM izdelia";
+            using (var mc = new MySqlCommand(sql, connect))
+            using(var reader = mc.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    var product = new Product
+                    {
+                        Name = reader.GetString("Name"),
+                        Description = reader.GetString("Description"),
+                        Price = reader.GetInt32("Price"),
+                        Amount = reader.GetInt32("Amount"),
+                        Size = reader.GetString("Size"),
+                        Breakable = reader.GetBoolean("Breakable")
+                    };
+                }
+            }
+            return products;
+        }
+    }
+}
